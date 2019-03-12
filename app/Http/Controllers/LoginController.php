b6025519ee91479ada3a4012735ce715af87349a
Your Name <you@example.com>
2019-03-12 22:14:06 +0800

    remove appid to .env

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -12,8 +12,8 @@ class LoginController extends Controller
     {
         $code = $request->code;
         $client = new Client;
-        $appid = 'wx549a7828c1343823';
-        $secret = 'd73f0b512dbf7d8bee3c3eaf969b78d3';
+        $appid = env('SMALL_PROGRAM_APPID');
+        $secret = env('SMALL_PROGRAM_SECRET');
 
          $url = "https://api.weixin.qq.com/sns/jscode2session?appid=".$appid."&secret=".$secret."&js_code=".$code."&grant_type=authorization_code";
         $response = $client->request('GET', $url)->getBody();
