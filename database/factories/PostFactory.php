899c7bf344ba696f84083850331e9c6b4c9f3f69
Your Name <you@example.com>
2019-06-02 21:09:33 +0800

    update seeder

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -8,6 +8,12 @@ $factory->define(App\Models\Post::class, function (Faker $faker) {
         'content' => '这是我的爱情宣言，这是我的爱情宣言，这是我的爱情宣言，这是我的爱情宣言，这是我的爱情宣言，这是我的爱情宣言，这是我的爱情宣言。',
         'gender' => array_rand(array_flip(['男', '女'])),
         'contact' => '88901236',
+        'nickname' => $faker->name,
+        'bod' => $faker->date($format = 'Y-m-d', $max = 'now'),
+        'location' => ['辽宁省', '大连市', '中山区'],
+        'marriage' => '未婚',
+        'hobby' => '运动,读书',
+
 
     ];
 });
