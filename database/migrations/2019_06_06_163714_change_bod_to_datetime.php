c5c09f83fd161698c2271b069c58d515a55724c8
Your Name <you@example.com>
2019-06-06 23:02:26 +0800

    modify migration

--- database/migrations/2019_06_06_163714_change_bod_to_datetime.php
+++ database/migrations/2019_06_06_163714_change_bod_to_datetime.php
@@ -14,7 +14,7 @@ class ChangeBodToDatetime extends Migration
     public function up()
     {
         Schema::table('posts', function (Blueprint $table) {
-            $table->dateTime('bod')->change();
+            //$table->dateTime('bod')->change();
         });
     }
 
