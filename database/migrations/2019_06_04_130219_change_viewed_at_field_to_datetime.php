e6497779c9c635baf675cf2da3390054105b6c13
Your Name <you@example.com>
2019-06-06 12:33:25 +0800

    text

--- database/migrations/2019_06_04_130219_change_viewed_at_field_to_datetime.php
+++ database/migrations/2019_06_04_130219_change_viewed_at_field_to_datetime.php
@@ -14,7 +14,7 @@ class ChangeViewedAtFieldToDatetime extends Migration
     public function up()
     {
         Schema::table('messages', function (Blueprint $table) {
-            $table->dateTime('viewed_at')->nullable()->change();
+            $table->dateTime('viewed_at')->change();
         });
     }
 
